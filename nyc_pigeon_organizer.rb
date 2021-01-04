14d6430c4ded906cb693c1b5ce893ff660d94513
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 03:43:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,9 +6,10 @@ def nyc_pigeon_organizer(data)
         names.each do |name|
           if !final_array[name]
             final_array[name] = {}
-        end
+          end
           if !final_array[name][key]
-            final_array[name][key] ={}
+            final_array[name][key] = []
+          end
     end
     final_array
   end
