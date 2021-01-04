70e07d32c5632b6f003d6d33e8f7787b6efc4df0
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 03:45:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,8 @@ def nyc_pigeon_organizer(data)
             final_array[name][key] = []
           end
           final_array[name][key].push(inner_key)
-    end
+        end
+      end
     final_array
   end
 end
